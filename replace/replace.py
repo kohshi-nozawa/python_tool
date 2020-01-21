7d439220a1d54389c298e9333491e79944739a02
kohshi.nozawa <k.nozawa@accenture.com>
2020-01-22 02:44:01 +0900

    add replace file

---
+++ replace/replace.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+import json
+
+f = open('./replace.json' , 'r')
+json_dict = json.load(f)
+
+for a in range(12):
+  print(a)
+
+for x in json_dict:
+  dist_page = x['body_value']
+  result_search = print('a11y-dialog.min\.js' in dist_page or 'photoswipe-ui-default\.min\.js' in dist_page)
+  
+  if result_search:
+    print(json_dict[x])
