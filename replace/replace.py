a3b8006e5cc8f0ef8ac032adb5ddc7a6971cd25d
kohshi.nozawa <kohshi.nozawa@webkn.site>
2020-01-22 03:07:26 +0900

    modified replace python

--- replace/replace.py
+++ replace/replace.py
@@ -5,9 +5,6 @@ import json
 f = open('./replace.json' , 'r')
 json_dict = json.load(f)
 
-for a in range(12):
-  print(a)
-
 for x in json_dict:
   dist_page = x['body_value']
   result_search = print('a11y-dialog.min.js' in dist_page or 'photoswipe-ui-default\.min.js' in dist_page)
